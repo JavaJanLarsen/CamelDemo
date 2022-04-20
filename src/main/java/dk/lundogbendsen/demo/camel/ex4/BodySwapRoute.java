96390c67166a65a1400044d1263fb2501cfc2956
Jan Larsen <jan@javacon.dk>
2022-04-20 20:51:02 +0200

    comment

--- src/main/java/dk/lundogbendsen/demo/camel/ex4/BodySwapRoute.java
+++ src/main/java/dk/lundogbendsen/demo/camel/ex4/BodySwapRoute.java
@@ -2,10 +2,6 @@ package dk.lundogbendsen.demo.camel.ex4;
 
 import org.apache.camel.builder.endpoint.EndpointRouteBuilder;
 
-/*
- * Lab14
- * This class is for testing using dk.lundogbendsen.camel.lab14.Lab14Test in the src\test\java folder
- */
 public class BodySwapRoute extends EndpointRouteBuilder {
 	 	
 	@Override
